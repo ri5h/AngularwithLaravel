c2ae7c2f1b0cedfb8aa57c19f3948477542c0df7
Rishiraj Purohit <purohitji007@gmail.com>
2017-01-15 14:44:56 +0530

    change file locations, test api status with laravel

--- backend/system/routes/api.php
+++ backend/system/routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::get('/status',function(Request $request){
+	$status = array("status"=>"success","msg"=>"Alive and Happy!!");
+	return $status;
+});
